ba32d259ec32be008070240cabcee6bb53fbb813
davifeitosa0 <davi.rodrigues@sptech.school>
2025-03-25 16:03:43 -0300

    Add EC2 and network modules for AWS infrastructure setup

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -8,19 +8,23 @@ resource "aws_key_pair" "generated_key" {
   public_key = file("id_rsa.pem.pub")
 }
 
+module "network" {
+  source = "../network"
+}
+
 resource "aws_instance" "app_server" {
+  tags = {
+    Name = "ec2-terraform"
+  }
+
   ami                    = "ami-084568db4383264d4"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.generated_key.key_name
-  vpc_security_group_ids = [aws_security_group.basic_security.id]
-  
+  subnet_id              = module.network.public_subnet_id
+  vpc_security_group_ids = [module.network.basic_security_group_id]
   ebs_block_device {
-    device_name = "dev/sda1"
+    device_name = "/dev/sda1"
     volume_size = 30
     volume_type = "standard"
-  }
-
-  tags = {
-    Name = "ec2-terraform"
   }
 }
